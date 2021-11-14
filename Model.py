73f39afdc4b4e4a19d32b84d42166c541ef5b70a
Melone, Aiden Michael <amelone3@gatech.edu>
2021-11-14 14:11:36 -0500

    Update Model.py
    Marginal updates

--- Model.py
+++ Model.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 import pandas as pd
+import math
 
 puzzlesdb = pd.read_csv("data/puzzle_data.csv", 
-                      names=["fen", "moves", "labels"])
+                    names=["fen", "moves", "labels"])
 puzzle_labels = puzzlesdb.pop("labels")
 
 #Allocates first 80% of database to training
-train_ind = len(puzzlesdb.index) * .8
+train_ind = math.floor(len(puzzlesdb.index) * .8)
 puzzle_train = puzzlesdb.head(train_ind)
 train_labels = puzzle_labels.head(train_ind)
 
@@ -16,7 +17,20 @@ puzzle_test = puzzlesdb.tail(test_ind)
 test_labels = puzzle_labels.tail(test_ind)
 
 #Builds the model
-model = tf.keras.Sequential([
-    
-])
+mlp = tf.keras.Sequential()
+mlp.add(tf.keras.Input(shape=(train_ind, )))
+mlp.add(tf.keras.layers.Dense(2, activation='relu'))
+mlp.add(tf.keras.layers.Dense(1, activation='relu'))
+#12 Classifications
+mlp.add(tf.keras.layers.Dense(12, activation='softmax'))
+mlp.summary()
 
+mlp.compile(optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+            )
+
+mlp.fit(puzzle_train, train_labels, epochs=30)
+test_loss, test_acc = mlp.evaluate(puzzle_test,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+print('\nTest loss:', test_loss)
