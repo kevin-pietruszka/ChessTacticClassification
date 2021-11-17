c090d2049c61a295a41fe81688db0bd8f013cfaf
oknauf <70602902+oknauf@users.noreply.github.com>
2021-11-17 00:53:33 -0500

    finish model

--- Model.py
+++ Model.py
@@ -4,28 +4,25 @@ import numpy as np
 import math
 
 def designModel(num):
-    # puzzlesdb = pd.read_csv("data/puzzle_data%s.csv" % num, 
-    #                     names=["fen", "moves", "labels"]).pop("labels")
-    puzzlesdb_view = np.load("data/matrix_rep%s.npz" % num, allow_pickle=True)
-    puzzle_labels = pd.read_csv("data/puzzle_data%s.csv" % num, 
-                        names=["fen", "moves", "labels"]).pop("labels")
+    puzzlesdb = np.load("data/matrix_rep%s.npz" % num, allow_pickle=True).values()
+    puzzle_labels = np.load("data/label%s.npz" % num, allow_pickle=True).values()
     print("Files Loaded")
     
-    puzzlesdb = np.empty((len(puzzlesdb_view.values()), num+1, 8, 8, 12))
-    i = 0
-    for puz in puzzlesdb_view.values():
-        puzzlesdb[i] = puz
-        i += 1
+    for p in puzzlesdb:
+        puzzlesdb = p
+
+    for pl in puzzle_labels:
+        puzzle_labels = pl
 
     #Allocates first 80% of database to training
     train_ind = math.floor(len(puzzlesdb) * .8)
     puzzle_train = puzzlesdb[:train_ind]
-    train_labels = puzzle_labels.head(train_ind)
+    train_labels = puzzle_labels[:train_ind]
 
     #Allocates the rest (20%) to testing
     test_ind = len(puzzlesdb) - train_ind
     puzzle_test = puzzlesdb[:test_ind]
-    test_labels = puzzle_labels.tail(test_ind)
+    test_labels = puzzle_labels[:test_ind]
     print("Local Vars Set")
 
     #Builds the model
@@ -33,20 +30,18 @@ def designModel(num):
     mlp.add(tf.keras.layers.Flatten(input_shape=(num+1, 8, 8, 12)))
     mlp.add(tf.keras.layers.Dense(128, activation='relu'))
     mlp.add(tf.keras.layers.Dense(64, activation='relu'))
-    mlp.add(tf.keras.layers.Dense(32, activation='relu'))
     #12 Classifications
-    mlp.add(tf.keras.layers.Dense(12, activation='softmax'))
+    mlp.add(tf.keras.layers.Dense(6, activation='softmax'))
     mlp.summary()
 
     mlp.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
+                loss='categorical_crossentropy',
                 metrics=['accuracy']
                 )
 
-
     mlp.fit(puzzle_train, train_labels, epochs=10)
-    test_loss, test_acc = mlp.evaluate(puzzle_test,  test_labels, verbose=2)
+    test_loss, test_acc = mlp.evaluate(puzzle_test, test_labels, verbose=2)
     print('\nTest accuracy:', test_acc)
     print('\nTest loss:', test_loss)
 
-designModel(12)
+designModel(2)
